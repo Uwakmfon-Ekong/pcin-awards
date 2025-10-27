136d392ebe0e2a1619fdeb30269e102403ab40ca
Uwakmfon-Ekong <uwakmfone470@gmail@example.com>
2025-10-27 17:22:32 +0100

    refactor: adjust navbar layout for improved spacing and alignment

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -24,11 +24,13 @@ export function Navigation() {
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50 backdrop-blur-lg bg-black/30 border-b border-white/10">
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-6 ">
-        {/* Logo */}
-        <Link href="/" className="flex items-center gap-2">
-          <Image src={logo} alt="PCIN Logo" className="w-30 h-30" />
-        </Link>
+  <div className="max-w-7xl mx-auto flex items-center justify-between px-4 py-2 md:px-6 md:py-4">
+    <Link href="/" className="flex items-center">
+      <Image src={logo} alt="PCIN Logo" className="h-20 w-auto md:h-30" priority />
+    </Link>
+    
+
+
 
         {/* Desktop Menu */}
         <div className="hidden md:flex items-center gap-8 text-white">
